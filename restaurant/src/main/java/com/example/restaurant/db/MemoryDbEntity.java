4dabe0c615387364492f0610055d34d6a13f8396
kimtaedong <rlaxoehd4234@naver.com>
2022-11-20 19:53:55 +0900

    2022/11/20

--- restaurant/src/main/java/com/example/restaurant/db/MemoryDbEntity.java
+++ restaurant/src/main/java/com/example/restaurant/db/MemoryDbEntity.java
@@ -1,2 +1,12 @@
-package com.example.restaurant.db;public class MemoryDbEntity {
+package com.example.restaurant.db;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MemoryDbEntity {
+    protected int index;
 }
--- restaurant/src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
+++ restaurant/src/main/java/com/example/restaurant/wishlist/repository/WishListRepository.java
@@ -1,2 +1,10 @@
-package com.example.restaurant.wishlist.repository;public class WishListRepository {
+package com.example.restaurant.wishlist.repository;
+
+import com.example.restaurant.db.MemoryDbRepositoryAbstract;
+import com.example.restaurant.db.MemoryDbRepositoryIfs;
+import com.example.restaurant.wishlist.entity.WishListEntity;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WishListRepository extends MemoryDbRepositoryAbstract<WishListEntity> {
 }
