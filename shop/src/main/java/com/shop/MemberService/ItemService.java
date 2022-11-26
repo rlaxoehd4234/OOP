39f46e2a1bef00ad1242c706101397f5460a0933
kimtaedong <rlaxoehd4234@naver.com>
2022-11-26 11:00:17 +0900

    2022/11/26

--- shop/src/main/java/com/shop/MemberService/ItemService.java
+++ shop/src/main/java/com/shop/MemberService/ItemService.java
@@ -1,18 +1,41 @@
 package com.shop.MemberService;
 
+import com.shop.dto.ItemFormDto;
+import com.shop.entity.Item;
+import com.shop.entity.ItemImg;
 import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class ItemService {  
+public class ItemService {
 
     private final ItemRepository itemRepository;
-    private final ItemImgService itemImgRepository;
+    private final ItemImgService itemImgService;
     private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto , List<MultipartFile> itemImgFileList) throws Exception{
+
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        for(int i = 0; i<itemImgFileList.size(); i++){
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+            if( i == 0)
+                itemImg.setRepImgYn("Y");
+            else
+                itemImg.setRepImgYn("N");
+            itemImgService.saveItemImg(itemImg,itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
 
 }
